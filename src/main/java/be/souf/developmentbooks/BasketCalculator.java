685575e5f151983c8b4ba1d837e4ba1fb230fe43
Soufiane Boulahya <souf.1060@gmail.com>
2023-04-27 13:40:44 +0200

    Impl discount for 10%
    - Refactoring code
    - New tests

--- src/main/java/be/souf/developmentbooks/BasketCalculator.java
+++ src/main/java/be/souf/developmentbooks/BasketCalculator.java
@@ -1,10 +1,38 @@
 package be.souf.developmentbooks;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class BasketCalculator {
 
     public static final double UNIT_PRICE = 50.0;
 
-    public double calculate(int quantityBook1, int quantityBook2) {
+    public double calculate(int... quantitiesByBook) {
+
+        double result = 0;
+        int nbDiscount = 0;
+
+        if (quantitiesByBook.length >= 3) {
+            nbDiscount = min(3, quantitiesByBook);
+            result += calculate(nbDiscount) * 3 * 0.90;
+        }
+        if (nbDiscount == 0 && quantitiesByBook.length >= 2) {
+            nbDiscount = min(2, quantitiesByBook);
+            result += calculate(nbDiscount) * 2 * 0.95;
+        }
+
+        for (int i = 0; i < quantitiesByBook.length; i++) {
+            if (quantitiesByBook[i] > nbDiscount) {
+                result += calculate(quantitiesByBook[i] - nbDiscount);
+
+            }
+        }
+
+        return result;
+    }
+
+    private double calculate(int quantityBook1, int quantityBook2) {
         int nbPossibleDiscount = Math.min(quantityBook1, quantityBook2);
 
         return calculate(quantityBook2 - nbPossibleDiscount) + calculate(quantityBook1 - nbPossibleDiscount)
@@ -13,6 +41,21 @@ public class BasketCalculator {
 
     private double calculate(int quantityBook) {
         return quantityBook * UNIT_PRICE;
+    }
+
+    private int min(int minSizeWithout0, int... values) {
+
+        List<Integer> valuesInSet =
+                IntStream
+                        .of(values)
+                        .filter(i -> i > 0)
+                        .mapToObj(Integer::valueOf)
+                        .collect(Collectors.toList());
+
+        if (valuesInSet.size() >= minSizeWithout0) {
+            return valuesInSet.stream().min(Integer::compare).get();
+        }
+        return 0;
     }
 
 }
--- src/test/java/be/souf/developmentbooks/BasketCalculatorTest.java
+++ src/test/java/be/souf/developmentbooks/BasketCalculatorTest.java
@@ -26,8 +26,10 @@ class BasketCalculatorTest {
                     "6,300"
             })
             void n_book(int quantity, double expectedResult) {
-                assertThat(tested.calculate(quantity, 0)).isEqualTo(expectedResult);
-                assertThat(tested.calculate(0, quantity)).isEqualTo(expectedResult);
+                assertThat(tested.calculate(quantity, 0, 0)).isEqualTo(expectedResult);
+                assertThat(tested.calculate(0, quantity, 0)).isEqualTo(expectedResult);
+                assertThat(tested.calculate(0, 0, quantity)).isEqualTo(expectedResult);
+
             }
 
         }
@@ -59,14 +61,87 @@ class BasketCalculatorTest {
                     "6,3,435",
                     "6,4,480",
                     "6,5,525",
-                    "6,6,570",
+                    "6,6,570"
             })
             void n_book_of_book1_and_m_book_of_book2(int quantityBook1, int quantityBook2, double expectedResult) {
-                assertThat(tested.calculate(quantityBook1, quantityBook2)).isEqualTo(expectedResult);
-                assertThat(tested.calculate(quantityBook2, quantityBook1)).isEqualTo(expectedResult);
+                assertThat(tested.calculate(quantityBook1, quantityBook2, 0)).isEqualTo(expectedResult);
+                assertThat(tested.calculate(quantityBook1, 0, quantityBook2)).isEqualTo(expectedResult);
+                assertThat(tested.calculate(0, quantityBook1, quantityBook2)).isEqualTo(expectedResult);
+
+                assertThat(tested.calculate(quantityBook2, quantityBook1, 0)).isEqualTo(expectedResult);
+                assertThat(tested.calculate(quantityBook2, 0, quantityBook1)).isEqualTo(expectedResult);
+                assertThat(tested.calculate(0, quantityBook2, quantityBook1)).isEqualTo(expectedResult);
+
             }
         }
 
+        @Nested
+        class DiscountOf10PerCent {
+            @ParameterizedTest
+            @CsvSource({
+                    "1,1,1,135",
+                    "2,1,1,185",
+                    "2,2,1,235",
+                    "2,2,2,270",
+                    "3,1,1,235",
+                    "3,2,1,285",
+                    "3,2,2,320",
+                    "3,3,1,335",
+                    "3,3,2,370",
+                    "3,3,3,405",
+                    "4,1,1,285",
+                    "4,2,1,335",
+                    "4,2,2,370",
+                    "4,3,1,385",
+                    "4,3,2,420",
+                    "4,3,3,455",
+                    "4,4,1,435",
+                    "4,4,2,470",
+                    "4,4,3,505",
+                    "4,4,4,540",
+                    "5,1,1,335",
+                    "5,2,1,385",
+                    "5,2,2,420",
+                    "5,3,1,435",
+                    "5,3,2,470",
+                    "5,3,3,505",
+                    "5,4,1,485",
+                    "5,4,2,520",
+                    "5,4,3,555",
+                    "5,4,4,590",
+                    "5,5,1,535",
+                    "5,5,2,570",
+                    "5,5,3,605",
+                    "5,5,4,640",
+                    "5,5,5,675",
+                    "6,1,1,385",
+                    "6,2,1,435",
+                    "6,2,2,470",
+                    "6,3,1,485",
+                    "6,3,2,520",
+                    "6,3,3,555",
+                    "6,4,1,535",
+                    "6,4,2,570",
+                    "6,4,3,605",
+                    "6,4,4,640",
+                    "6,5,1,585",
+                    "6,5,2,620",
+                    "6,5,3,655",
+                    "6,5,4,690",
+                    "6,5,5,725",
+                    "6,6,1,635",
+                    "6,6,2,670",
+                    "6,6,3,705",
+                    "6,6,4,740",
+                    "6,6,5,775",
+                    "6,6,6,810"
+            })
+            void n_book_of_book1_and_m_book_of_book2(int quantityBook1, int quantityBook2, int quantityBook3,
+                    double expectedResult) {
+                assertThat(tested.calculate(quantityBook1, quantityBook2, quantityBook3)).isEqualTo(expectedResult);
+                assertThat(tested.calculate(quantityBook2, quantityBook1, quantityBook3)).isEqualTo(expectedResult);
+            }
+        }
     }
 
 }
