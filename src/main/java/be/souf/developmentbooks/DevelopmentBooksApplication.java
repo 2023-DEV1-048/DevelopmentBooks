b198b7ce6e04f6a541cd822c5551faae9e9c0b88
Soufiane Boulahya <souf.1060@gmail.com>
2023-04-27 17:58:46 +0200

    Add spring-boot dependencies
    Minimal config

---
+++ src/main/java/be/souf/developmentbooks/DevelopmentBooksApplication.java
@@ -0,0 +1,12 @@
+package be.souf.developmentbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopmentBooksApplication.class, args);
+    }
+}
