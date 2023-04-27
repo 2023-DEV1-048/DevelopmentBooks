3cbe841223e4e9928cd9034c97e61cd010948bfd
Soufiane Boulahya <souf.1060@gmail.com>
2023-04-27 11:22:38 +0200

    Implementation (only same copy is taken)
    - Fix junit 5 dependency
    - Downgrade Java version to 11 (17 is not installed in my machine)

--- src/main/java/be/souf/developmentbooks/BasketCalculator.java
+++ src/main/java/be/souf/developmentbooks/BasketCalculator.java
@@ -2,4 +2,10 @@ package be.souf.developmentbooks;
 
 public class BasketCalculator {
 
+    private static final int UNIT_PRICE = 50;
+
+    public int calculate(int i) {
+        return i * UNIT_PRICE;
+    }
+
 }
--- src/test/java/be/souf/developmentbooks/BasketCalculatorTest.java
+++ src/test/java/be/souf/developmentbooks/BasketCalculatorTest.java
@@ -1,16 +1,45 @@
 package be.souf.developmentbooks;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-public class BasketCalculatorTest {
+class BasketCalculatorTest {
 
     private final BasketCalculator tested = new BasketCalculator();
 
-    @Test
-    public void test() {
-        fail("Not yet implemented");
+    @Nested
+    class SameCopyOfBook {
+        @Test
+        void one_copy() {
+            assertThat(tested.calculate(1)).isEqualTo(50);
+        }
+
+        @Test
+        void two_copies() {
+            assertThat(tested.calculate(2)).isEqualTo(100);
+        }
+
+        @Test
+        void three_copies() {
+            assertThat(tested.calculate(3)).isEqualTo(150);
+        }
+
+        @Test
+        void four_copies() {
+            assertThat(tested.calculate(4)).isEqualTo(200);
+        }
+
+        @Test
+        void five_copies() {
+            assertThat(tested.calculate(5)).isEqualTo(250);
+        }
+
+        @Test
+        void six_copies() {
+            assertThat(tested.calculate(6)).isEqualTo(300);
+        }
     }
 
 }
