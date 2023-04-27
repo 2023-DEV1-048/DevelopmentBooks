15745bf3ca9752f6b7bb1379949d65a5a0525bb7
Soufiane Boulahya <souf.1060@gmail.com>
2023-04-27 20:19:06 +0200

    Use RestController + Swagger

--- src/main/java/be/souf/developmentbooks/frontend/web/BasketCalculatorController.java
+++ src/main/java/be/souf/developmentbooks/frontend/web/BasketCalculatorController.java
@@ -1,20 +1,24 @@
 package be.souf.developmentbooks.frontend.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import be.souf.developmentbooks.frontend.BasketCalculatorService;
 
-@Controller
+@RestController
 public class BasketCalculatorController {
 
     @Autowired
     private BasketCalculatorService delegate;
 
-    @GetMapping("/calculate")
-    public double calculate(int... i) {
-        return delegate.calculate(i);
+    @CrossOrigin(origins = "*")
+    @GetMapping(path = "/calculate", produces = MediaType.APPLICATION_JSON_VALUE)
+    public double calculate(@RequestParam(name = "quantity") int... quantity) {
+        return delegate.calculate(quantity);
     }
 
 }
